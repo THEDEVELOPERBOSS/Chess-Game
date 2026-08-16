2ff8de17ee2edf74d56a76bce89e24ee1d516317
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-08-15 20:30:57 -0600

    Added variabales for piece images and locations

--- main.py
+++ main.py
@@ -7,17 +7,71 @@ pygame.init()
 WIDTH = 1000
 HEIGHT = 900
 screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('Two-Player Pygame Chess!')
 font = pygame.font.Font('freesansbold.ttf', 20)
 big_font = pygame.font.Font('freesansbold.ttf', 50)
 timer = pygame.time.Clock()
 fps = 60 
 # Game variables and images 
-
+white_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook', 
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn'] # Keeps track of all pieces on board
+white_locations = [(0, 0), (1, 0), (2, 0), (3, 0), (4, 0), (5, 0), (6, 0), (7, 0)
+                   (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1)] # Where the pieces are. Top left is 0,0, bottom right is 7,7
+black_pieces = ['rook', 'knight', 'bishop', 'king', 'queen', 'bishop', 'knight', 'rook', 
+                'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn', 'pawn']
+black_locations = [(0, 7), (1, 7), (2, 7), (3, 7), (4, 7), (5, 7), (6, 7), (7, 7)
+                   (0, 6), (1, 6), (2, 6), (3, 6), (4, 6), (5, 6), (6, 6), (7, 6)]
+captured_pieces_white = []
+captured_pieces_black = []
+# 0 - whites turn, no selection; 1 - whites turn, piece selected; 2 - blacks turn, no selection; 3 - blacks turn, piece selected 
+turn_step = 0
+selection = 100 # Stores what piece location is selected. When no piece is selected make it big enough so others aren't accidently selected
+valid_moves = [] 
+# load in game piece images (queen, king, rook, bishop, knight, pawn) x2
+black_queen = pygame.image.load('assets/images/black queen.png')
+black_queen = pygame.image.scale(black_queen, (80, 80)) # Bigger pieces need a bigger board, smaller pieces smaller board
+black_queen_small = pygame.image.scale(black_queen, (45, 45)) # For when piece is taken and is on the side
+black_king = pygame.image.load('assets/images/black king.png')
+black_king = pygame.image.scale(black_king, (80, 80)) 
+black_king_small = pygame.image.scale(black_king, (45, 45))
+black_bishop = pygame.image.load('assets/images/black bishop.png')
+black_bishop = pygame.image.scale(black_bishop, (80, 80)) 
+black_bishop_small = pygame.image.scale(black_bishop, (45, 45))
+black_pawn = pygame.image.load('assets/images/black pawn.png')
+black_pawn = pygame.image.scale(black_pawn, (80, 80)) 
+black_pawn_small = pygame.image.scale(black_pawn, (45, 45))
+black_rook = pygame.image.load('assets/images/black rook.png')
+black_rook = pygame.image.scale(black_rook, (80, 80)) 
+black_rook_small = pygame.image.scale(black_rook, (45, 45))
+white_bishop = pygame.image.load('assets/images/white bishop.png')
+white_bishop = pygame.image.scale(white_bishop, (80, 80)) 
+white_bishop_small = pygame.image.scale(white_bishop, (45, 45))
+white_king = pygame.image.load('assets/images/white king.png')
+white_king = pygame.image.scale(white_king, (80, 80)) 
+white_king_small = pygame.image.scale(white_king, (45, 45))
+white_knight = pygame.image.load('assets/images/white knight.png')
+white_knigt = pygame.image.scale(white_knight, (80, 80)) 
+white_knight_small = pygame.image.scale(white_knight, (45, 45))
+white_pawn = pygame.image.load('assets/images/white pawn.png')
+white_pawn = pygame.image.scale(white_pawn, (80, 80)) 
+white_pawn_small = pygame.image.scale(white_pawn, (45, 45))
+white_queen = pygame.image.load('assets/images/white queen.png')
+white_queen = pygame.image.scale(white_queen, (80, 80)) 
+white_queen_small = pygame.image.scale(white_queen, (45, 45))
+white_rook = pygame.image.load('assets/images/white rook.png')
+white_rook = pygame.image.scale(white_rook, (80, 80)) 
+white_rook_small = pygame.image.scale(white_rook, (45, 45))
+# Time stamp 18:29 
 # main game loop 
 run = True
 while run:
     timer.tick(fps)
     screen.fill('dark gray') # Background color 
     
+    # Event handling
     for event in pygame.event.get(): # gets keyboard, mouse, etc from computer 
-        
+        if event.type == pygame.QUIT: # Checks to see if X at top of game window was clicked
+            run = False
+            
+    pygame.display.flip()
+pygame.quit()
