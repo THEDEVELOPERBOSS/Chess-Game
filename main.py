70e75d0fb89a55f1500c2f446341851523f90f17
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-08-22 11:32:23 -0600

    Added timestamp

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ def def_pieces():
        for i in range(len(white_pieces)): # can't just say 16. Needs to check how many pieces are actually on the board
         index = piece_list.index(white_pieces[i]) # gets the index of whatever piece is currently being looked at
         if white_pieces[i] == 'pawn': # have to do a seperate for pawns because they are different sizes
-            screen.blit(white_pawn, (white_locations))
+            screen.blit(white_pawn, (white_locations)) # time stamp 36:48
         
                       
 # main game loop 
