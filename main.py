bc6f2a0ba004e016b319c984e170c5daa8f865fe
THEDEVELOPERBOSS <109550555+THEDEVELOPERBOSS@users.noreply.github.com>
2026-09-19 16:14:22 -0600

    working on pawn moves

--- main.py
+++ main.py
@@ -143,9 +143,32 @@ def check_options(pieces, locations, turn):
     return all_moves_list
 
 
-#
-
-                   
+# check valid pawn moves
+def check_pawn(position, color):
+    moves_list = []
+    if color == 'white':
+        if(position[0], position[1] + 1) not in white_locations and \
+                (position[0], position[1] + 1) not in black_locations and position[1] < 7:
+            moves_list.append((position[0], position[1] + 1))
+        if(position[0], position[1] + 2) not in white_locations and \
+                (position[0], position[1] + 2) not in black_locations and position[1] == 1:
+            moves_list.append((position[0], position[1] + 2))
+        if (position[0] + 1, position[1] + 1) in black_locations:
+            moves_list.append((position[0] + 1, position[1] + 1))
+        if (position[0] - 1, position[1] + 1) in black_locations: # checks to the left
+            moves_list.append((position[0] - 1, position[1] + 1))
+            
+    if color == 'black':
+        if(position[0], position[1] + 1) not in white_locations and \
+                (position[0], position[1] + 1) not in black_locations and position[1] < 7:
+            moves_list.append((position[0], position[1] + 1))
+        if(position[0], position[1] + 2) not in white_locations and \
+                (position[0], position[1] + 2) not in black_locations and position[1] == 1:
+            moves_list.append((position[0], position[1] + 2))
+        if (position[0] + 1, position[1] + 1) in black_locations:
+            moves_list.append((position[0] + 1, position[1] + 1))
+        if (position[0] - 1, position[1] + 1) in black_locations: # checks to the left
+            moves_list.append((position[0] - 1, position[1] + 1)) # 1:05:39
 # main game loop 
 black_options = check_options(black_pieces, black_locations, 'black') # intially populate black and white options list 
 white_options = check_options(white_pieces, white_locations, 'white')
